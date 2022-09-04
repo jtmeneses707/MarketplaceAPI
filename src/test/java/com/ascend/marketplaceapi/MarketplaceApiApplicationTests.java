4926305516ee250bbdfcb790a19a49151bdbafd7
jtmeneses707 <jtmenese@gmail.com>
2022-09-04 11:13:34 -0700

    chore: init commit

---
+++ src/test/java/com/ascend/marketplaceapi/MarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascend.marketplaceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
